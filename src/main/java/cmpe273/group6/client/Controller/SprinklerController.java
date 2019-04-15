99526158f3fbe85fca990cc9c5177fb5794af8ad
Daniel Yang <achillesyang@hotmail.com>
2019-04-15 13:39:30 -0700

    Sprinkler up to bs
    bs complete with sprinkler

--- src/main/java/cmpe273/group6/client/Controller/SprinklerController.java
+++ src/main/java/cmpe273/group6/client/Controller/SprinklerController.java
@@ -4,6 +4,12 @@ import cmpe273.group6.client.Entity.Sprinkler;
 import cmpe273.group6.client.Service.SprinklerRepository;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @RestController
 @RequestMapping("/sprinklers")
 public class SprinklerController {
@@ -17,14 +23,106 @@ public class SprinklerController {
     // Get all sprinklers
     @GetMapping
     public @ResponseBody
-    Iterable<Sprinkler> getAllSensors() {
+    Iterable<Sprinkler> getAllSprinklers() {
         return sprinklerRepository.findAll();
     }
 
     // Create a single sprinkler.
     @PostMapping
-    public Sprinkler createSensor(@RequestBody Sprinkler sprinkler) {
+    public Sprinkler createSprinkler(@RequestBody Sprinkler sprinkler) {
         return sprinklerRepository.save(sprinkler);
     }
 
+    // Get a single sprinkler.
+    @GetMapping("/{id}")
+    public Sprinkler getSprinklerById(@PathVariable(value="id") long sprinklerId) {
+        // .orElseThrow(() -> new ResourceNotFoundException("Sprinkler", "Id", sprinklerId))
+        return sprinklerRepository.findSprinklerById(sprinklerId);
+    }
+
+    // update a sprinkler.
+    @PutMapping(value = "/{id}")
+    public Sprinkler updateSprinkler(@PathVariable(value="id") long sprinklerId, @Valid @RequestBody Sprinkler sprinklerDetails) {
+        Sprinkler sprinkler = sprinklerRepository.findSprinklerById(sprinklerDetails.getId());
+        sprinkler.setId(sprinklerDetails.getId());
+        sprinkler.setState(sprinklerDetails.isState());
+        Sprinkler updateSprinkler = sprinklerRepository.save(sprinkler);
+        return sprinkler;
+    }
+
+    // Delete a sprinkler.
+    @DeleteMapping("/{id}")
+    public String deleteSprinkler (@PathVariable (value="id") long sprinklerId) {
+        if (sprinklerRepository.findSprinklerById(sprinklerId) == null) {
+            return "The sprinkler is not being bootstrapped, please check!";
+        }
+
+        Sprinkler sprinkler = sprinklerRepository.findSprinklerById(sprinklerId);
+        String access_server = sprinkler.getAuth() + "/sprinklers/" + sprinklerId;
+        StringBuffer response = new StringBuffer();
+        try {
+            URL urlObj = new URL(access_server);
+            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestMethod("DELETE");
+            con.getResponseMessage();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (response.toString().equals("Delete Succeed")) {
+            sprinklerRepository.delete(sprinkler);
+            return "Delete success";
+        }
+        return "Something is wrong with your deletion.";
+    }
+
+    // Bootstrap a sensor.
+    @GetMapping("/bs/{id}")
+    public String bootStrapSprinkler(@PathVariable(value = "id") long sprinklerId) {
+        // System.out.println(sensorDetails.getId());
+        Sprinkler sprinkler = sprinklerRepository.findSprinklerById(sprinklerId);
+        String bs_fid = sprinkler.getFid();
+        // send Bootstrap request operation to bootstrap server
+        String url = "http://localhost:8091/bs/" + bs_fid;
+        StringBuffer response = new StringBuffer();
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestMethod("GET");
+            con.getResponseMessage();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (response.toString().substring(0,4).equals("http")) {
+            sprinkler.setAuth(response.toString());
+            sprinklerRepository.save(sprinkler);
+
+            return "Bootstrap Succeed.";
+        } else {
+            return response.toString();
+        }
+    }
 }
--- src/main/java/cmpe273/group6/client/Entity/Sprinkler.java
+++ src/main/java/cmpe273/group6/client/Entity/Sprinkler.java
@@ -4,6 +4,7 @@ package cmpe273.group6.client.Entity;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Entity
@@ -18,16 +19,28 @@ public class Sprinkler implements Serializable {
     @Column (name = "state")
     private boolean state;
 
-    public Sprinkler(int id, boolean state) {
+    @Column(name = "access_server")
+    private String auth;
+
+    @Column(name = "fid")
+    private String fid;
+
+    public Sprinkler () {
+
+    }
+
+    public Sprinkler(int id, boolean state, @NotBlank String fid) {
         this.id = id;
         this.state = state;
+        this.auth = "";
+        this.fid = fid;
     }
 
     public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -37,5 +50,21 @@ public class Sprinkler implements Serializable {
 
     public void setState(boolean state) {
         this.state = state;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+
+    public String getFid() {
+        return fid;
+    }
+
+    public void setFid(String fid) {
+        this.fid = fid;
     }
 }
